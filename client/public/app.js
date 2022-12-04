22d6464aeabbbaa76707dd17bc03e8669f4761ec
isaguimet <isabellaguimet@hotmail.com>
2022-12-03 18:37:25 -0700

    Clicking on "Add list" creates a new list button

--- client/public/app.js
+++ client/public/app.js
@@ -1,4 +1,22 @@
 // client-side JS code
 const socket = io();
 
+let addListButton = document.getElementById('add-list-button');
+addListButton.addEventListener('click', function () {
+    // Gets all list buttons created
+    let buttonsCreated = document.getElementsByClassName("btn btn-primary");
+    let lastButtonCreated = document.getElementsByClassName("btn btn-primary")[buttonsCreated.length-1];
+    lastButtonCreated.style.marginBottom = "10px";
+    console.log(lastButtonCreated);
+
+    // Position this newly created list button after the last created list button
+    lastButtonCreated.insertAdjacentHTML("afterend",
+    "<p>" +
+            "<button class='btn btn-primary' type='button' data-bs-toggle='collapse' " +
+                "data-bs-target='#collapseExample' aria-expanded='false' aria-controls='collapseExample' contenteditable='true'>" +
+                "myList" +
+            "</button>" +
+        "</p>");
+})
+
 console.log("hello from client");
