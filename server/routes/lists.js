2c5c6f188ac8747207735dddaf5aa561ff2d2ffe
ViTsang <47119514+ViTsang@users.noreply.github.com>
2022-12-04 21:22:37 -0700

    finished post requests

--- server/routes/lists.js
+++ server/routes/lists.js
@@ -12,13 +12,15 @@ const List = require("../models/list.model");
 
 //Create a new list given a user ID it belongs to
 router.route("/new-list").post((req, res) => {
-    User.findOne({ _id: req.body.userID})
+    User.findOne({ username: req.body.username})
         .then(user => {
             if(user === null){
                 res.status(400).json("Error : No user with ID ${req.body.userID} exists");
             }else {
                 const newList = new List({
-                    listName: req.body.listName, budget: req.body.budget, creatorID: req.body.userID
+                    listName: req.body.listName, 
+                    budget: req.body.budget, 
+                    creatorID:username
                 });
                 newList.save()
                     .then(() => res.status(200).json)
@@ -30,13 +32,13 @@ router.route("/new-list").post((req, res) => {
 
 //Change list name
 router.route("/:listID").post((req, res) => {
-    List.updateOne({ _id: req.params.listID}, { $set: {listName: req.body.item}})
+    List.updateOne({ _id: req.params.listID}, { $set: {listName: req.body.listName}})
         .then(() => res.status(200).json())
         .catch(err => res.status(500).json("Error: " + err));
 });
 
 //Change budget of list
-router.route("/change-budget/:listID").post((req, res) => {
+router.route("/update-budget/:listID").post((req, res) => {
     List.updateOne({ _id: req.params.itemID}, { $set: {budget: req.body.budget}})
         .then(() => res.status(200).json())
         .catch(err => res.status(500).json("Error: " + err));
@@ -48,11 +50,7 @@ router.route("/add-user/:listID").post((req, res) => {
     List.updateOne(
         { _id: req.params.listID}, 
         {$set: 
-            {shared: 
-                {user: req.body.user, 
-                permission: req.body.user.permission
-                }
-            }
+            {shared: {user: req.body.user, permission: req.body.user.permission} }
         }
     )
         .then(() => res.status(200).json())
@@ -66,39 +64,26 @@ router.route("/add-user/:listID").post((req, res) => {
     GET REQUESTS
 */
 
-
 //Get all the lists of the user
-router.route("/user/:username").get((req, res) => {
-    User.findOne({ _id: req.body.userID})
-    .then( user => {
-        List.find()
-        .then(lists => res.json(lists))
-        .catch(err => res.status(400).json("Error: " + err));
+router.route("/get-user-lists/:username").get((req, res) => {
+    List.find({
+        $or: [ {"creator": username}, {"shared.user": username}]
     })
+        .then(lists => res.status(200).json(lists))
+        .catch(err => res.status(400).json("Error: " + err))
 });
 
-//Get a specific list of the user
-router.route("/user/:username").get((req, res) => {
-    User.findOne({ _id: req.body.userID})
-    .then( user => {
-        List.findOne({ _id: req.body.listID})
-        .then( list => {
+//Get a specific list
+router.route("/:listID").get((req, res) => {
+    List.findOne({ _id: req.params.listID})
+        .then(list => {
             if (list === null) res.status(400).json(list)
-            else res.status(200).json(list)
-        })
-        .catch(err => res.status(400).json("Error: " + err));
-    })
-});
-
-//Get budget of list
-router.route("/user/:username").get((req, res) => {
-    User.findOne({username: req.params.username})
-        .then(user => {
-            if (user === null) res.status(400).json(user)
             else res.status(200).json(user)
         })
         .catch(err => res.status(500).json("Error: " + err));
 });
+
+//Get budget of list
 
 //Get total value of list
 
@@ -118,10 +103,5 @@ router.route("/:listID").delete((req, res) => {
 });
 
 //Delete a user
-router.route("/:listID").delete((req, res) => {
-    List.remove({ _id: req.params.listID})
-        .then(() => res.status(200).json())
-        .catch(err => res.status(500).json("Error: " + err));
-});
 
 module.exports = router;
